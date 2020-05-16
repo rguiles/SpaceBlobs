a8b5de09c569c53cb816e70fb92e51ea2a374d8d
kurorose19 <60154891+kurorose19@users.noreply.github.com>
2020-05-16 11:03:37 -0700

    Update Assets

---
+++ GameOver.js
@@ -0,0 +1,18 @@
+class GameOver extends Phaser.Scene {
+  constructor() {
+    super('GameOver');
+  }
+
+  init() {
+  }
+
+  preload() {
+  }
+
+  create() {
+    this.config = this.game.config;
+  }
+
+  update() {
+  }
+}
